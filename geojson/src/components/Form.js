fcc25dbfdace85479b02244356ce46ae7ecf27a1
rafał-Komputer\rafał <rafallukasik123@wp.pl>
2019-05-30 19:05:18 +0200

    szkielet projektu

---
+++ geojson/src/components/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+class Form extends Component {
+  render() {
+    return (
+      <header>
+      <h1>Weather app</h1>
+      <p>Check the weather for any place in the world! </p>
+      </header>
+    );
+  }
+}
+
+export default Form;
---
+++ geojson/src/components/Table.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Form from './Form';
+
+
+class Table extends Component {
+  render() {
+    return (
+     
+     <Form />
+    );
+  }
+}
+
+export default Table;
